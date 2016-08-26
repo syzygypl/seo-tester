5ce6a6c6d2e43c2205f414aa3108d9d151d88e68
Andrzej Duś <andrzej.dus@arsthanea.com>
2016-08-26 21:05:36 +0200

    Minor cleanup in some rules

--- rules/imagesWithAlt.js
+++ rules/imagesWithAlt.js
@@ -1,7 +1,7 @@
 module.exports = ($, errors) => {
   $('img').each((index, element) => {
-    const alt = element ? $(element).prop('alt') : '';
-    if (alt.length === 0) {
+    const alt = $(element).prop('alt');
+    if (!alt || alt.length === 0) {
       const cssClass = $(element).prop('class');
       const src = $(element).prop('src');
       errors.push(`There is no alt in img tag, class: ${cssClass}, src: ${src}`);
--- rules/linksWithTitle.js
+++ rules/linksWithTitle.js
@@ -1,7 +1,7 @@
 module.exports = ($, errors) => {
   $('a').each((index, element) => {
-    const title = element ? $(element).prop('title') : '';
-    if (title.length === 0) {
+    const title = $(element).prop('title');
+    if (!title || title.length === 0) {
       const cssClass = $(element).prop('class');
       const href = $(element).prop('href');
       errors.push(`Link has no title, class: ${cssClass}, href: ${href}`);
