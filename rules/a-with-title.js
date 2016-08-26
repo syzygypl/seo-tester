b98dfa1bac13c00ee84953d1cb4d537201fbd948
Andrzej Duś <andrzej.dus@arsthanea.com>
2016-08-26 12:38:03 +0200

    More verbose error messages

--- rules/a-with-title.js
+++ rules/a-with-title.js
@@ -2,8 +2,9 @@ module.exports = ($, errors) => {
   $('a').each((index, element) => {
     const title = element ? $(element).prop('title') : '';
     if (title.length === 0) {
+      const cssClass = $(element).prop('class');
       const href = $(element).prop('href');
-      errors.push(`Link has no title, href: ${href}`);
+      errors.push(`Link has no title, class: ${cssClass}, href: ${href}`);
     }
   });
 };
--- rules/img-with-alt.js
+++ rules/img-with-alt.js
@@ -2,8 +2,9 @@ module.exports = ($, errors) => {
   $('img').each((index, element) => {
     const alt = element ? $(element).prop('alt') : '';
     if (alt.length === 0) {
+      const cssClass = $(element).prop('class');
       const src = $(element).prop('src');
-      errors.push(`There is no alt in img tag, src: ${src}`);
+      errors.push(`There is no alt in img tag, class: ${cssClass}, src: ${src}`);
     }
   });
 };
